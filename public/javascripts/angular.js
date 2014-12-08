3e075df7b9551b5c98204dbfc3802806bf5fa6db
nikitabu <boutakovn@gmail.com>
2014-12-08 00:40:35 -0800

    angular models working, pulling from mongodb

--- public/javascripts/angular.js
+++ public/javascripts/angular.js
@@ -1,9 +1,42 @@
 (function(){
     var app = angular.module('drudeLorentzApp', []);
 
-    app.controller('materialSelector', function(){});
+    app.controller('materialController', function($scope, $http){
+	$scope.materials = null;
 
-    app.controller('wavelengthSelector', function(){});
+	$http.get('/material')
+	     .success( function(data) {
+		 $scope.materials = data;
+	     })
+	    .error( function(data, status, headers, config){
+		console.log('error retreiving materials');
+	    })
+
+	$scope.currentMaterial = null;
+
+	$scope.editCurrentMaterial = function(item) {
+	    $scope.currentMaterial = item;
+	    console.log("current material = "+$scope.currentMaterial.name);
+	}
+    });
+
+    app.controller('plotController', function($scope, $http){
+	$scope.plot = [
+	    { 'wmin' : 2,
+	      'wmax' : 16,
+	      'parameter' : n // n = refractive index, e = permittivity
+	    }
+	];
+
+	$http.get('/plotparameters')
+	     .success( function(data) {
+		 $scope.plot = data;
+	     })
+	    .error( function(data, status, headers, config){
+		console.log('error retreiving plot parameters');
+	    })
+    });
+
 
     app.directive('materialParameters',function(){});
 
